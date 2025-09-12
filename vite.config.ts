c371913e243aefc5a5527a0731f6f4d4b85a542b
Yuuta <yuutaman0424ku@gmail.com>
2025-09-12 21:09:19 +0900

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "/tornado-02-front/",
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), VitePWA({
-    registerType: "autoUpdate",
-    injectRegister: 'auto',
-    devOptions: {
-      enabled: true
-    },
-    manifest: {
-      name: "Viofolio",
-      short_name: "Viofolio",
-      description: "Viofolio - 学習量ではなく学習を計測するアプリ",
-      theme_color: "#5A4A8A",
-      background_color: "#6B569D",
-      start_url: "/tornado-02-front/",
-      icons: [
-        {
-          src: "viofolio_192.png",
-          sizes: "192x192",
-          type: "image/png"
-        }
-      ]
-    }
-  })],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   build: {
     outDir: "build",
   },
